ea62f33f0ca86321725bd6f39ec9dd5407425afe
cheekiemonkiez <167571818+cheekiemonkiez@users.noreply.github.com>
2024-04-19 14:12:35 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwoCodingChallenge
+{
+    internal class Player
+    {
+        private string name;
+        private int score;
+        private int livesLeft;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public Player(string name, int startingLives) 
+        {
+            this.name = name;
+            livesLeft = startingLives;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public void AddPoints(int totalPoints)
+        {
+            score += totalPoints;
+        }
+
+        public void kill()
+        {
+            if (livesLeft > 0) 
+            {
+                livesLeft--;
+            }
+        }
+
+        public int GetLivesLeft()
+        {
+            return livesLeft;
+        }
+    }
+}
